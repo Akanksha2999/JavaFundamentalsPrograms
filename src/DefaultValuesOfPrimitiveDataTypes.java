24924ffaa7e188e56bdf77af69b4509867d397ed
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-02 19:16:44 +0530

    Added: Displayed default values of primitive data types

---
+++ src/DefaultValuesOfPrimitiveDataTypes.java
@@ -0,0 +1,22 @@
+public class DefaultValuesOfPrimitiveDataTypes {
+    static boolean b;
+    static double d;
+    static float f;
+    static int i;
+    static long l;
+    static char c;
+    static short s;
+    static byte by;
+
+    public static void main(String[] args) {
+        System.out.println("Byte:" +b);
+        System.out.println("Double:" +d);
+        System.out.println("Float:" +f);
+        System.out.println("Int:" +i);
+        System.out.println("Long:" +l);
+        System.out.println("Char:" +c);
+        System.out.println("Short:" +s);
+        System.out.println("Byte:" +by);
+    }
+
+}
