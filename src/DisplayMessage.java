3be7975c45a5ed873bcd488653fb3557e12bdd15
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-02 19:02:09 +0530

    Executed: Displayed message

---
+++ src/DisplayMessage.java
@@ -0,0 +1,7 @@
+public class DisplayMessage {
+    public static void main(String[] args) {
+
+        //Welcome message printed
+        System.out.println("Welcome to day1 practice programs");
+    }
+}
